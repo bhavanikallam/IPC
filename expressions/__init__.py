4f3f3a2b5cb79de481b4b6641a20af2e5fa4fbed
BhavaniKallam <36656934+bhavanikallam@users.noreply.github.com>
2022-07-12 15:01:59 +0530

    Add files via upload

---
+++ expressions/__init__.py
@@ -0,0 +1,6 @@
+from apps.IPCApp.expressions.datefn import *
+from apps.IPCApp.expressions.aggfn import *
+from apps.IPCApp.expressions.mathfn import *
+from apps.IPCApp.expressions.statisticalfn import *
+from apps.IPCApp.expressions.textfn import *
+from apps.IPCApp.expressions.functions import *
