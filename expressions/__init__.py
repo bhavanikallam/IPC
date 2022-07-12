c4ef00b4ec4d0bdc0614253d0e78499cf1175a87
BhavaniKallam <36656934+bhavanikallam@users.noreply.github.com>
2022-07-12 15:08:36 +0530

    Delete expressions directory

--- expressions/__init__.py
+++
@@ -1,6 +0,0 @@
-from apps.IPCApp.expressions.datefn import *
-from apps.IPCApp.expressions.aggfn import *
-from apps.IPCApp.expressions.mathfn import *
-from apps.IPCApp.expressions.statisticalfn import *
-from apps.IPCApp.expressions.textfn import *
-from apps.IPCApp.expressions.functions import *
